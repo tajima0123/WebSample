830bf88d0c80fce2694cabff5d0c875f32154cec
0801JA <0801JA@DESKTOP-VPQADDH>
2023-09-15 15:20:52 +0900

    9月15日

---
+++ WebSample001/src/main/java/pkg01/Servlet01.java
@@ -0,0 +1,39 @@
+package pkg01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet01
+ */
+@WebServlet("/Servlet01")
+public class Servlet01 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<meta charset=\"UTF-8\">");
+		out.println("<title>TEST</title>");
+		out.println("</html>");
+		out.println("<body>");
+		out.println("<h2>サーブレットの確認</h2>");
+		out.println("</body>");
+		out.println("</head>");
+	}
+
+}
